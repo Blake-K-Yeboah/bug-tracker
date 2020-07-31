65138d0c76bea8fd32486506eea66e3e4a5f5a0d
Blake Yeboah <blakekyeboah@gmail.com>
2020-07-31 22:12:39 +1000

    Added Passport Config + Database Connection

--- server.js
+++ server.js
@@ -1,8 +1,49 @@
 const express = require('express');
 const app = express();
 
+// Require Mongoose for DB
+const mongoose = require('mongoose');
+
+// Require Body Parser
+const bodyParser = require('body-parser');
+
+// Require passport
+const passport = require('passport');
+
+// Require User Model
+const User = require('./models/user.model');
+
+// Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(bodyParser.json());
+
+// Test Request
 app.get('/', (req, res) => res.send("Hello There!"));
 
+// DB Config
+const db = process.env.MONGOURI || require("./config/keys").mongoURI;
+
+// Connect to MongoDB 
+mongoose
+    .connect(
+        db,
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Port Environment Variable or 5000
 const port = process.env.PORT || '5000';
 
-app.listen(port, () => console.log('Server Listening on port: ' + port))
+app.listen(port, () => console.log(`Server Listening on Port: ${port}`));
