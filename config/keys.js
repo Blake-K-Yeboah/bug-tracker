6aaa0a9fb8c5caf77efd2f285293d820266018a8
Blake Yeboah <blakekyeboah@gmail.com>
2020-07-31 22:13:19 +1000

    Added Config File

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://dbAdmin:dbAdminPassword@main-cluster.5cn92.mongodb.net/development?retryWrites=true&w=majority",
+    secretOrKey: "This is a secret!"
+}
