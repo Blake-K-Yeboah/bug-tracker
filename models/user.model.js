eeb34cfa4a30604e7e6e0db3c35d04f3c68f3128
Blake Yeboah <blakekyeboah@gmail.com>
2020-07-31 22:13:28 +1000

    Added User Model

---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now
+    },
+    profileIcon: {
+        type: String,
+        default: 'default.jpg'
+    }
+});
+
+// Export Model
+module.exports = User = mongoose.model("users", UserSchema);         
