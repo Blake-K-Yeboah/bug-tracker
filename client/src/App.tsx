105f69e83e102c9425be8beb014796eb653c9eb5
Blake Yeboah <blakekyeboah@gmail.com>
2020-08-01 18:03:22 +1000

    Added Basic Routing and store provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+
+// Import Stores
+import { authStore } from './stores/authStore';
+
+// Import Provider
+import { Provider } from 'mobx-react';
+
+// Import React Router Dom Stuff
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom'
+
+// Import Pages
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
+    <Router>
+
+      <Provider authStore={authStore}>
+
+        <Switch>
+
+          <Route exact path="/" component={Home} />
+
+        </Switch>
+
+      </Provider>
+
+    </Router>
   );
 }
 
