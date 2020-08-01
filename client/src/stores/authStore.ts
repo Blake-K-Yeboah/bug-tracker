5ab3266faadce3ad2f28b56081aea84cb36b888f
Blake Yeboah <blakekyeboah@gmail.com>
2020-08-01 18:01:54 +1000

    Added Home Store

---
+++ client/src/stores/authStore.ts
@@ -0,0 +1,44 @@
+import { observable, action } from 'mobx';
+
+// Import JWT decode to decode jwt token
+import jwt_decode from 'jwt-decode';
+
+import { Iuser, IAuthStore } from '../types';
+
+export class authstore {
+
+    // Store JWT Token
+    @observable token: string | null = document.cookie.split('=')[1];
+
+    // Store authenticated boolean
+    @observable isAuthenticated = this.token ? true : false;
+
+    // Store Logged in User
+    @observable user = this.token ? jwt_decode(this.token) : null;
+
+    // Store Error
+    @observable error: string | null = null;
+
+    // Action to change error
+    @action
+    setError(err: string) {
+        this.error = err;
+    }
+
+    // Action to set current user
+    @action
+    setCurrentUser(user: Iuser | null) {
+        this.user = user;
+        if (user === null) this.isAuthenticated = false;
+    }
+
+    // Action to set token
+    @action
+    setToken(token: string | null) {
+        this.token = token;
+        this.isAuthenticated = token ? true : false;
+    }
+
+}
+
+export const authStore: IAuthStore = new authstore();
