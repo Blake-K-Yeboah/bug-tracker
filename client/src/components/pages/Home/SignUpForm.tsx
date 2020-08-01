1ba458d581e394c0c2d15f70b2db540918d82d8e
Blake Yeboah <blakekyeboah@gmail.com>
2020-08-01 22:29:08 +1000

    Created Sign Up Form Component

---
+++ client/src/components/pages/Home/SignUpForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SignUpForm = () => {
+    return (
+        <div className="sign-up">
+
+            <h1 className="title">Sign Up</h1>
+
+            <p className="sub-text">Fill out the following form to sign up</p>
+
+        </div>
+    )
+}
+
+export default SignUpForm
