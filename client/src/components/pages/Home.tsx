41c865c074a78153877e42b6a556afee7bd8a34f
Blake Yeboah <blakekyeboah@gmail.com>
2020-08-01 18:03:45 +1000

    Added Home Page

---
+++ client/src/components/pages/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Home = () => {
+    return (
+        <>
+
+            <Helmet>
+
+                <title>Bug Tracker - Home</title>
+
+            </Helmet>
+
+            <h1>Home</h1>
+
+        </>
+    )
+}
+
+export default Home
