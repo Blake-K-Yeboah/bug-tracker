61d8005bd190a7f5af04cebc42f75fd8f5d7aa73
Blake Yeboah <blakekyeboah@gmail.com>
2020-08-28 20:31:06 +1000

    Created Dashboard Page

---
+++ client/src/components/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Dashboard = () => {
+    return (
+        <>
+
+            <Helmet>
+
+                <title>BugTracker - Dashboard</title>
+
+            </Helmet>
+
+            <h1>Dashboard</h1> 
+
+        </>
+    )
+}
+
+export default Dashboard
