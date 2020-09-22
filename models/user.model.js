8a982a64232789158df1d72d36aebc8cc9180670
Blake Yeboah <blakekyeboah@gmail.com>
2020-09-22 16:49:36 +1000

    Added User Role Property

--- models/user.model.js
+++ models/user.model.js
@@ -19,6 +19,10 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
+    role: {
+        type: String,
+        default: 'submitter'
+    },
     createdOn: {
         type: Date,
         default: Date.now
