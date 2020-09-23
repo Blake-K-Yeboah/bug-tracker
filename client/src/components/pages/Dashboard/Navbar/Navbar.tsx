0f6dc48856bccfcaba05474afb0afd504ce32b92
Blake Yeboah <blakekyeboah@gmail.com>
2020-09-23 20:58:22 +1000

    Added Menu Icon + Dropdown Component

--- client/src/components/pages/Dashboard/Navbar/Navbar.tsx
+++ client/src/components/pages/Dashboard/Navbar/Navbar.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Import Styling
 import './Navbar.scss';
 
+import { BsGrid3X3Gap } from 'react-icons/bs';
+
+import Dropdown from './Dropdown/Dropdown';
+
 const Navbar = () => {
+
+    const [show, setShow] = useState(false);
 
     return (
         <nav className="navbar">
 
             <h1 className="navbar-brand">Bug Tracker</h1>
+
+            <BsGrid3X3Gap className="menu-icon" onClick={() => setShow(!show)} />
+
+            <Dropdown display={show} />
 
         </nav>
     )
