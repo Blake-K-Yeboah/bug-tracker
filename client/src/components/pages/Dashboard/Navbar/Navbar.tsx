321539ecaaafc5cc16a1b0a5ebad36e9202a0a1a
Blake Yeboah <blakekyeboah@gmail.com>
2020-09-23 19:19:18 +1000

    Created Navbar Component

---
+++ client/src/components/pages/Dashboard/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// Import Styling
+import './Navbar.scss';
+
+const Navbar = () => {
+
+    return (
+        <nav className="navbar">
+
+            <h1 className="navbar-brand">Bug Tracker</h1>
+
+        </nav>
+    )
+
+}
+
+export default Navbar
