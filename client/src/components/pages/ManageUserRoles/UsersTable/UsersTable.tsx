481fc37e80726c9c1a61b05bfccf66312ea083f6
Blake Yeboah <blakekyeboah@gmail.com>
2020-09-24 19:41:01 +1000

    Created Users Table Component

---
+++ client/src/components/pages/ManageUserRoles/UsersTable/UsersTable.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Import Styling
+import './UsersTable.scss';
+
+const UsersTable = () => {
+    return (
+        <div>
+            <h1>Users Table</h1>
+        </div>
+    )
+}
+
+export default UsersTable
