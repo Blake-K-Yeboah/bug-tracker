14355856d6b5c0984445edb215f18a4441674096
Blake Yeboah <blakekyeboah@gmail.com>
2020-09-25 15:43:43 +1000

    Created Table

--- client/src/components/pages/ManageUserRoles/UsersTable/UsersTable.tsx
+++ client/src/components/pages/ManageUserRoles/UsersTable/UsersTable.tsx
@@ -1,14 +1,59 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 // Import Styling
 import './UsersTable.scss';
 
-const UsersTable = () => {
+// Import MOBX Stuff
+import { inject, observer } from 'mobx-react';
+
+// Import types
+import { IStoreProps, Iuser } from '../../../../types';
+
+// Import Spinner for loading
+import Spinner from '../../../Spinner/Spinner';
+import TableRow from './TableRow';
+
+let UsersTable = ({ usersStore }: IStoreProps) => {
+
+    useEffect(() => {
+        usersStore.fetchUsers();
+    }, [usersStore]);
+
     return (
-        <div>
-            <h1>Users Table</h1>
+        <div className="users-table-container">
+
+            <h2 className="table-title">Table of Users</h2>
+
+            {!usersStore.users ? <Spinner /> :
+                <table className="users-table">
+                    <thead>
+                        <tr className="head-row">
+                            <th className="t-head">
+                                Name
+                            </th>
+                            <th className="t-head">
+                                Email
+                            </th>
+                            <th className="t-head">
+                                Created
+                            </th>
+                            <th className="t-head">
+                                Role
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {usersStore.users.map((user: Iuser) => {
+                            return <TableRow user={user} key={user._id} />
+                        })}
+                    </tbody>
+                </table>
+            }
+
         </div>
     )
 }
+
+UsersTable = inject('usersStore')(observer(UsersTable));
 
 export default UsersTable
