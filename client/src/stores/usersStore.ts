2a3e9c0f9563ebbd3d72d0f6029a7df988d85f8b
Blake Yeboah <blakekyeboah@gmail.com>
2020-09-25 13:42:25 +1000

    Added Fetch Users Action

--- client/src/stores/usersStore.ts
+++ client/src/stores/usersStore.ts
@@ -2,9 +2,21 @@ import { observable, action } from 'mobx';
 import { Iuser } from '../types';
 import axios from 'axios';
 
-export class usersStore {
+export class usersstore {
 
     // Store users 
     @observable users: Iuser[] | null = null;
 
+    @action fetchUsers() {
+        axios.get('/api/users').then(res => {
+
+            if (res.data.length > 0) {
+                this.users = res.data;
+            }
+
+        });
+    }
+
 }
+
+export const usersStore = new usersstore();
