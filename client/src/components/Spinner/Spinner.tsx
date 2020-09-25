8f95ffc116fade9f2d93bb60642d4181ac7f8c20
Blake Yeboah <blakekyeboah@gmail.com>
2020-09-25 15:42:38 +1000

    Created Spinner Component

---
+++ client/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// Import Styling
+import './Spinner.scss';
+
+// Import icon
+import { FaSpinner } from 'react-icons/fa';
+
+const Spinner = () => {
+    return (
+        <>
+            <FaSpinner className="spinner" />
+        </>
+    )
+}
+
+export default Spinner
