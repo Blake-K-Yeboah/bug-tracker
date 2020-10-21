00ebae116b5e8238935637723c5d79194de94476
Blake Yeboah <blakekyeboah@gmail.com>
2020-10-21 15:18:37 +1000

    Added View All Link

--- client/src/components/pages/Dashboard/LatestActivity/LatestActivity.tsx
+++ client/src/components/pages/Dashboard/LatestActivity/LatestActivity.tsx
@@ -10,6 +10,7 @@ import { inject, observer } from 'mobx-react';
 import { IStoreProps } from '../../../../types';
 
 import LatestActivityList from './LatestActivityList/LatestActivityList';
+import { NavLink } from 'react-router-dom';
 
 let LatestActivity = ({ changeStore }: IStoreProps) => {
 
@@ -23,6 +24,10 @@ let LatestActivity = ({ changeStore }: IStoreProps) => {
             <h3 className="heading">Latest Activity</h3>
 
             <LatestActivityList />
+
+            {
+                changeStore.changeCount > 7 ? <NavLink to="/history" className="view-all-link">View All</NavLink> : <span className="disabled-view-all-link">View All</span>
+            }
 
         </div>  
     )
