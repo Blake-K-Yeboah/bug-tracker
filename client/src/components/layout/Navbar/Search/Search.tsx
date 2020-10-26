a7415483f156d4c70701dc56b4dcdf5ce132b723
Blake Yeboah <blakekyeboah@gmail.com>
2020-10-26 20:25:40 +1000

    Created Search Component

---
+++ client/src/components/layout/Navbar/Search/Search.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Import Styling
+import './Search.scss';
+
+const Search = () => {
+    return (
+        <div className="search">
+          <input type="text" className="search-input" placeholder="Search"/>
+        </div>
+    )
+}
+
+export default Search
