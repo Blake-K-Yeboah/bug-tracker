861a53f5a8da289049a1f3c79b7ca0aaf631494a
Blake Yeboah <blakekyeboah@gmail.com>
2020-10-27 16:29:37 +1000

    Added GET route'

--- routes/api/projects.routes.js
+++ routes/api/projects.routes.js
@@ -4,8 +4,33 @@ const express = require('express');
 // Define Router
 const router = express.Router();
 
+// Import Project Model
+const Project = require("../../models/project.model");
+
 router.get('/:id?', (req, res) => {
-    return res.send("Projects");
+    
+    let id = req.params.id;
+
+    if (id) {
+
+        // Return Individual project if there is an id
+        Project.findById(id).then(project => {
+
+            // If there is no project return error
+            if (!project) return res.status(400).json({ error: "Project Does Not Exist" });
+
+            return res.json(project);
+
+        });
+
+    } else {
+
+        // Return all projects
+        Project.find({}).then(projects => {
+            return res.json(projects);
+        });
+
+    }
 });
 
 module.exports = router;
