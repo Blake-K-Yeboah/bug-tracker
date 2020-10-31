c6c5da961e857bb3db89c6dae12517bd7989c8fb
Blake Yeboah <blakekyeboah@gmail.com>
2020-10-31 22:55:03 +1000

    Created ProjectActionsBar component

---
+++ client/src/components/pages/Projects/ProjectActionsBar/ProjectActionsBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Import Styling
+import './ProjectActionsBar.scss';
+
+const ProjectActionsBar = () => {
+    return (
+        <div className="project-actions-bar">
+            
+        </div>
+    )
+}
+
+export default ProjectActionsBar
