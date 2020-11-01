6c5effc41844411981a9c696dfd67da8da422dc2
Blake Yeboah <blakekyeboah@gmail.com>
2020-11-01 16:54:34 +1000

    Added Sort Inputs, Filter input and create new project button

--- client/src/components/pages/Projects/ProjectActionsBar/ProjectActionsBar.tsx
+++ client/src/components/pages/Projects/ProjectActionsBar/ProjectActionsBar.tsx
@@ -1,14 +1,61 @@
-import React from 'react'
+import { inject, observer } from 'mobx-react';
+import React, { useEffect } from 'react'
+import { FaPlus } from 'react-icons/fa';
+import { IStoreProps, Iuser } from '../../../../types';
 
 // Import Styling
 import './ProjectActionsBar.scss';
 
-const ProjectActionsBar = () => {
+let ProjectActionsBar = ({ usersStore }: IStoreProps) => {
+    
+    useEffect(() => {
+        usersStore.fetchUsers();
+    }, [usersStore]);
+
     return (
         <div className="project-actions-bar">
             
+            <label htmlFor="sort-select">Sort By</label>
+
+            <select className="sort-select" id="sort-select">
+
+                <option value="a-z">Name (A-Z)</option>
+                <option value="z-a">Name (Z-A)</option>
+                <option value="newest-first">Newest First</option>
+                <option value="oldest-first">Oldest First</option>
+
+            </select>
+
+            <label htmlFor="owner-filter">Filter by Owner</label>
+
+            <select id="owner-filter">
+
+                <option value="all">All</option>
+                        
+                {usersStore.userCount === 0 ? '' : usersStore.users.map((user: Iuser) => {
+                        
+                    if (user.role === "project-manager" || user.role === "admin") {
+                        return (
+                            <option value={user.name} key={user._id}>{user.name}</option>
+                        )
+                    } else {
+                        return ''
+                    }
+
+                })}
+
+            </select>
+
+            <div className="new-project-section">
+
+                <button className="btn primary has-icon">Create New Project <FaPlus className="icon" /></button>
+                
+            </div>
+
         </div>
     )
 }
 
-export default ProjectActionsBar
+ProjectActionsBar = inject("usersStore")(observer(ProjectActionsBar));
+
+export default ProjectActionsBar;
