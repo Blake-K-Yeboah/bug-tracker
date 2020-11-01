b43b09e3ab2c2723184a0b64cb9c1354623fb76d
Blake Yeboah <blakekyeboah@gmail.com>
2020-11-01 20:42:51 +1000

    Created Project List Component

---
+++ client/src/components/pages/Projects/ProjectList/ProjectList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './ProjectList.scss';
+
+const ProjectList = () => {
+    return (
+        <div className="project-list-container">
+            
+            <h2 className="heading">Projects</h2>
+            
+        </div>
+    )
+}
+
+export default ProjectList
