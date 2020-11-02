a9e5a2f66090f8d1025e9eb9e91e6ce51c2fb85f
Blake Yeboah <blakekyeboah@gmail.com>
2020-11-02 16:42:54 +1000

    Added POST route for creating projects

--- routes/api/projects.routes.js
+++ routes/api/projects.routes.js
@@ -6,8 +6,17 @@ const keys = require("../../config/keys");
 // Define Router
 const router = express.Router();
 
+// Validation Functions
+const validateProjectInput = require("../../validation/project");
+
 // Import Project Model
 const Project = require("../../models/project.model");
+
+// Import Change Model
+const Change = require('../../models/change.model');
+
+// Import User Model
+const User = require('../../models/user.model');
 
 router.get('/:id?', jwt({ secret: keys.secretOrKey, algorithms: ['HS256'] }), (req, res) => {
     
@@ -33,6 +42,61 @@ router.get('/:id?', jwt({ secret: keys.secretOrKey, algorithms: ['HS256'] }), (r
         });
 
     }
+
+});
+
+router.post('/create', jwt({ secret: keys.secretOrKey, algorithms: ['HS256'] }), (req, res) => {
+
+    // Validation
+    const { errors, isValid } = validateProjectInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    Project.findOne({ name: req.body.name }).then(project => {
+
+        // Check if project exists with same name
+        if (project) {
+            res.status(400).json({ name: 'Project with same name exists'});
+        } else {
+
+            // Check owner is admin or project
+            User.findById(req.body.userId).then(user => {
+                if (!user) {
+                    return res.status(400).json({ owner: "No user with that ID" });
+                } else if (user.role != "admin" && user.role != 'project-manager') {
+                    return res.status(401).json({ owner: "You dont have permission." });
+                }
+            });
+
+            // Create New Project Model
+            const newProject = new Project({
+                name: req.body.name,
+                description: req.body.description,
+                owner: req.body.userId
+            });
+
+            newProject.save().then(project => {
+                const properties = {
+                    userId: project.owner,
+                    projectName: project.name
+                }
+
+                const newChange = new Change({
+                    message: "created a project called ",
+                    type: "PROJECT_CREATED",
+                    properties: JSON.stringify(properties)
+                });
+
+                newChange.save().then(change => { console.log(change) }).catch(err => console.log(err));
+
+                return res.json(project);
+
+            })
+        }
+    });
 
 });
 
