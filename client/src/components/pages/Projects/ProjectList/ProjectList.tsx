0ecfbb9012a885106cc9cb1bb7ee3c8262a281da
Blake Yeboah <blakekyeboah@gmail.com>
2020-11-04 19:30:32 +1000

    Updated Project List

--- client/src/components/pages/Projects/ProjectList/ProjectList.tsx
+++ client/src/components/pages/Projects/ProjectList/ProjectList.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import { inject, observer } from 'mobx-react';
+import React, { useEffect } from 'react'
+import { IStoreProps } from '../../../../types';
 
 import './ProjectList.scss';
 
-const ProjectList = () => {
+import ProjectItem from './ProjectItem/ProjectItem';
+import Spinner from '../../../Spinner/Spinner';
+
+let ProjectList = ({ projectStore }: IStoreProps) => {
+
+    useEffect(() => {
+        projectStore.fetchProjects();
+    }, []);
+
     return (
         <div className="project-list-container">
             
-            <h2 className="heading">Projects</h2>
+            <h2 className="heading">Table of Projects ({projectStore.projectCount} project{projectStore.projectCount === 1 ? '' : 's'})</h2>
             
+            {projectStore.projectCount === 0 ? <Spinner /> : 'Project Table'}
+
         </div>
     )
 }
 
-export default ProjectList
+ProjectList = inject("projectStore")(observer(ProjectList));
+
+export default ProjectList;
