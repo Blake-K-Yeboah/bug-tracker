e8f67816148f3f9b3fc3268766d5c2fa07ccd6cf
Blake Yeboah <blakekyeboah@gmail.com>
2020-11-05 21:08:06 +1000

    Added table of projects

--- client/src/components/pages/Projects/ProjectList/ProjectList.tsx
+++ client/src/components/pages/Projects/ProjectList/ProjectList.tsx
@@ -1,24 +1,47 @@
 import { inject, observer } from 'mobx-react';
 import React, { useEffect } from 'react'
-import { IStoreProps } from '../../../../types';
+import { Iproject, IStoreProps } from '../../../../types';
 
 import './ProjectList.scss';
 
-import ProjectItem from './ProjectItem/ProjectItem';
 import Spinner from '../../../Spinner/Spinner';
+import TableRow from './TableRow';
 
 let ProjectList = ({ projectStore }: IStoreProps) => {
 
     useEffect(() => {
         projectStore.fetchProjects();
-    }, []);
+    }, [projectStore]);
 
     return (
         <div className="project-list-container">
             
             <h2 className="heading">Table of Projects ({projectStore.projectCount} project{projectStore.projectCount === 1 ? '' : 's'})</h2>
             
-            {projectStore.projectCount === 0 ? <Spinner /> : 'Project Table'}
+            {projectStore.projectCount === 0 ? <Spinner /> 
+            :  <table className="projects-table">
+                    <thead>
+                        <tr className="head-row">
+                            <th className="t-head">
+                                Name
+                            </th>
+                            <th className="t-head">
+                                Description
+                            </th>
+                            <th className="t-head">
+                                Owner
+                            </th>
+                            <th className="t-head">
+                                Project Link
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {projectStore.projects.map((project: Iproject) => {
+                            return <TableRow project={project} key={project._id} />
+                        })}
+                    </tbody>
+                </table>}
 
         </div>
     )
