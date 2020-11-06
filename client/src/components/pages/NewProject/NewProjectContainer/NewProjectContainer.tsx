741d1b0a2e63eb59da086890838f800d8e8400cc
Blake Yeboah <blakekyeboah@gmail.com>
2020-11-06 13:49:57 +1000

    Created NewProjectContainer Component

---
+++ client/src/components/pages/NewProject/NewProjectContainer/NewProjectContainer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './NewProjectContainer.scss';
+
+const NewProjectContainer = () => {
+    return (
+        <div className="new-project-container">
+            
+            <h2 className="heading">Create New Project</h2>
+
+            <p className="sub-text">Fill out the following form to create a new project</p>
+        </div>
+    )
+}
+
+export default NewProjectContainer
