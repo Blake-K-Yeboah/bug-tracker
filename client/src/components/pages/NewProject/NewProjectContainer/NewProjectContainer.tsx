14a330cb19292ed5db0d66b5f4f5d3e0e2d6c9d0
Blake Yeboah <blakekyeboah@gmail.com>
2020-11-09 14:55:46 +1000

    Added New Project Form

--- client/src/components/pages/NewProject/NewProjectContainer/NewProjectContainer.tsx
+++ client/src/components/pages/NewProject/NewProjectContainer/NewProjectContainer.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { inject, observer } from 'mobx-react';
+import React, { useEffect, useState } from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { IStoreProps, Iuser } from '../../../../types';
 
 // Imported Styling
 import './NewProjectContainer.scss';
 
-const NewProjectContainer = () => {
+let NewProjectContainer = ({ usersStore }: IStoreProps) => {
+
+    useEffect(() => {
+        usersStore.fetchUsers();
+    }, [usersStore]);
+
+    const [userInput, setUserInput] = useState({
+        title: '',
+        description: ''
+    });
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserInput({ ...userInput, [e.target.id]: e.target.value});
+    }
+
     return (
         <div className="new-project-container">
             
@@ -11,8 +28,59 @@ const NewProjectContainer = () => {
 
             <p className="sub-text">Fill out the following form to create a new project</p>
             
+            <div className="form-container new-project-form-container">
+
+                <form className="new-project-form form">
+
+                    <div className="form-group">
+
+                        <label className="input-label" htmlFor="title">Title</label>
+                        <input type="text" className="input" placeholder="Title: " id="title" onChange={onChange} value={userInput.title} />
+
+                    </div>
+
+                    <div className="form-group">
+
+                        <label className="input-label" htmlFor="description">Short Description</label>
+                        <input type="text" className="input" placeholder="Description: " id="description" onChange={onChange} value={userInput.description} />
+
+                    </div>
+
+                    <div className="form-group">
+
+                        <label className="input-label">Owner</label>
+                        <select className="select">
+
+                        {usersStore.userCount === 0 ? '' : usersStore.users.map((user: Iuser) => {
+                        
+                            if (user.role === "project-manager" || user.role === "admin") {
+                                return (
+                                    <option value={user.name} key={user._id}>{user.name}</option>
+                                )
+                            } else {
+                                return ''
+                            }
+        
+                        })}
+
+                        </select>
+
+                    </div>
+                    
+                    <div className="form-group">
+
+                        <button type="submit" className="btn primary has-icon">Create <FiPlus className="icon" /></button>
+
+                    </div>
+
+                </form>
+
+            </div>
+
         </div>
     )
 }
 
-export default NewProjectContainer
+NewProjectContainer = inject("usersStore")(observer(NewProjectContainer));
+
+export default NewProjectContainer;
