407613123b251afef39326ca52a89d4bf07c264c
Blake Yeboah <blakekyeboah@gmail.com>
2020-11-15 20:42:33 +1000

    Created User Profile Page

---
+++ client/src/components/pages/UserProfile/UserProfile.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+
+// Import Helmet to access document head
+import { Helmet } from 'react-helmet'
+
+// Import Page Components
+import Sidebar from '../../layout/Sidebar/Sidebar'
+import Navbar from '../../layout/Navbar/Navbar';
+import Axios from 'axios';
+
+const UserProfile: any = ({ match }: any) => {
+
+    const userId = match.params.id;
+
+    const [user, setUser]: any = useState(null);
+
+    useEffect(() => {
+
+        Axios.get(`/api/users/${userId}`).then(res => {
+            setUser(res.data);
+        }).catch(err => console.error(err));
+
+    }, [userId]);
+
+    return (
+        <>
+            <Helmet>
+
+                <title>{user ? `Bug Tracker - ${user.name}` : 'Loading'}</title>
+
+            </Helmet>
+
+            <div className="page-content">
+
+                <Navbar />
+
+                <Sidebar />
+
+                <h1 className="page-title">User Profile: {user ? user.name : ''}</h1>
+                
+            </div>
+
+        </>
+    )
+}
+
+export default UserProfile
