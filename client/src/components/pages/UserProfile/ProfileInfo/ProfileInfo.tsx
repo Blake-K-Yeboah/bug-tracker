a13bb94ad12456edf02e9fdb75a53f0b729ca7ad
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-01 00:56:03 +1000

    Added Details to Profile Info Section

--- client/src/components/pages/UserProfile/ProfileInfo/ProfileInfo.tsx
+++ client/src/components/pages/UserProfile/ProfileInfo/ProfileInfo.tsx
@@ -6,16 +6,52 @@ import './ProfileInfo.scss';
 // Import Image
 import profileHeader from './profile-header.jpg';
 
+// Import Icons
+import { FaEnvelope, FaUser } from 'react-icons/fa';
+
 const ProfileInfo: any = ({ user }: any) => {
 
+    const role = user && user.role === 'project-manager' ? 'Project Manager' : 
+                 user ? `${user.role.charAt(0).toUpperCase()}${user.role.slice(1,user.role.length)}` : 
+                 '';
+
     return (
+
         <div className="profile-info">
             
-            <img src={profileHeader} alt="Header Image" className="header-img" />
+            <img src={profileHeader} alt="Header" className="header-img" />
 
+            { user ? <>
+                    <img src={`${process.env.PUBLIC_URL}/uploads/profile/${user.profileIcon}`} alt="Profile Icon" className="profile-img" />
+
+                    <h3 className="user-name">{user.name}</h3>
+
+                    <p className="user-bio">{user.bio}</p>
+
+                    <div className="user-icon-group">
+
+                        <FaEnvelope className="icon" />
+
+                        <span className="text">{user.email}</span>
+
+                    </div>
+
+                    <div className="user-icon-group">
+
+                        <FaUser className="icon" />
+
+                        <span className="text">{role}</span>
+
+                    </div>
+
+                </> :
+
+                ''
             
+            }
 
         </div>
+
     )
 }
 
