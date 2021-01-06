61021cdd0ecac64dbcf8be257ccad52a81c7021c
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-06 21:38:09 +1000

    Created Comments Component

---
+++ client/src/components/pages/ProjectPage/Comments/Comments.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// Import Styling
+import './Comments.scss';
+
+const Comments = ({ project }: any) => {
+
+    return (
+        <div className="comment-section">
+
+            <h2 className="title">Comments</h2>
+
+        </div>
+    )
+}
+
+export default Comments
