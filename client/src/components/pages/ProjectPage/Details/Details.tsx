d5d47fd50668ea735ea87bfcef1ff97c882898de
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-07 22:49:20 +1000

    Removed useState Import

--- client/src/components/pages/ProjectPage/Details/Details.tsx
+++ client/src/components/pages/ProjectPage/Details/Details.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import './Details.scss';
 import { inject, observer } from 'mobx-react';
 import { Iuser } from '../../../../types';
