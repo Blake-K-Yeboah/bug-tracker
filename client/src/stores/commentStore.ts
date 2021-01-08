a9607812fd6410cf3fcaf389c3b5718b38846dbd
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-08 12:47:08 +1000

    Created Comment Store

---
+++ client/src/stores/commentStore.ts
@@ -0,0 +1,32 @@
+import { observable, action } from 'mobx';
+import axios from 'axios';
+import { Icomment, ICommentStore } from '../types';
+
+export class commentstore {
+
+    // Store Comments
+    @observable comments: Icomment[] = [];
+
+    // Fetch Comments
+    @action fetchComments() {
+        axios.get('/api/comments').then(res => {
+            
+            if (res.data.length > 0) {
+
+                this.comments = res.data.map((comment: any) => {
+                    return {
+                        ...comment,
+                        for: JSON.parse(comment.for)
+                    }
+                });
+
+                console.log(this.comments);
+
+            }
+
+        })
+    }
+
+}
+
+export  const commentStore: ICommentStore = new commentstore();
