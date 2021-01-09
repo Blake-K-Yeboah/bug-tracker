6b3b37c388ba8077131e34c758bfbbf80207b2a4
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-09 19:14:41 +1000

    Created New Comment Component

---
+++ client/src/components/pages/ProjectPage/Comments/NewComment/NewComment.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './NewComment.scss';
+
+const NewComment = () => {
+    return (
+        <div className="new-comment">
+            
+            <h4 className="small-heading">Write a comment</h4>
+        </div>
+    )
+}
+
+export default NewComment
