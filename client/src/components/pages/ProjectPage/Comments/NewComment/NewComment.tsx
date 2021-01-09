278d9c06522cbe91b4f19edcb874c9638c8fead0
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-09 19:32:22 +1000

    Updated New Comment Component

--- client/src/components/pages/ProjectPage/Comments/NewComment/NewComment.tsx
+++ client/src/components/pages/ProjectPage/Comments/NewComment/NewComment.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './NewComment.scss';
 
 const NewComment = () => {
+
+    const [text, setText] = useState('');
+
+    const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setText(e.target.value);
+    }
+
     return (
         <div className="new-comment">
             
             <h4 className="small-heading">Write a comment</h4>
+
+            <input type="text" className="comment-input" value={text} onChange={inputChangeHandler} placeholder="Comment: " />
+
+            <button className="btn primary">Post</button>
+
         </div>
     )
 }
