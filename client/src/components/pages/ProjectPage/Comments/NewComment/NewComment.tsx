bb20aa59a11f137974a2696911d15603a1419612
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-09 19:46:04 +1000

    Created Post Button Handler

--- client/src/components/pages/ProjectPage/Comments/NewComment/NewComment.tsx
+++ client/src/components/pages/ProjectPage/Comments/NewComment/NewComment.tsx
@@ -2,13 +2,35 @@ import React, { useState } from 'react'
 
 import './NewComment.scss';
 
-const NewComment = () => {
+import { IStoreProps } from '../../../../../types';
+import { inject, observer } from 'mobx-react';
+import Axios from 'axios';
+
+let NewComment = ({ project, authStore, commentStore }: IStoreProps) => {
 
     const [text, setText] = useState('');
 
     const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setText(e.target.value);
     }
+
+    const createNewComment = () => {
+
+        const requestBody = {
+            text,
+            userId: authStore.user.id,
+            type: 'project',
+            typeId: project._id
+        }
+
+        Axios.post('/api/comments/create', requestBody).then(res => {
+            commentStore.fetchComments();
+            setText('')
+        }).catch(err => {
+            alert(err.response.data.msg);
+        });
+
+    };
 
     return (
         <div className="new-comment">
@@ -17,10 +39,12 @@ const NewComment = () => {
 
             <input type="text" className="comment-input" value={text} onChange={inputChangeHandler} placeholder="Comment: " />
 
-            <button className="btn primary">Post</button>
+            <button className="btn primary" onClick={createNewComment}>Post</button>
 
         </div>
     )
 }
+
+NewComment = inject('authStore', 'commentStore')(observer(NewComment));
 
 export default NewComment
