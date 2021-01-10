0788e6b9171df08f2c72a274db2e82e1b3ef4a81
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-10 21:22:12 +1000

    Created Add Button

--- client/src/components/pages/ProjectPage/AssignedUsers/AssignedUsers.tsx
+++ client/src/components/pages/ProjectPage/AssignedUsers/AssignedUsers.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 // Import Styling
 import './AssignedUsers.scss';
 
+// Import Icons
+import { FaPlus } from 'react-icons/fa';
+
 const AssignedUsers = ({ project }: any) => {
     return (
         <div className="assigned-users">
@@ -10,9 +13,10 @@ const AssignedUsers = ({ project }: any) => {
             {project ? <>
             
                 <h2 className="title">Assigned Users</h2>
-            
+                <button className="btn primary add-btn"><FaPlus className="icon" /></button>
+                
             </> : ''}
-            
+
         </div>
     )
 }
