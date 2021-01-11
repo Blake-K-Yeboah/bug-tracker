7fbb01d281e2d4f717619020df2366e3a7ad2e8b
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-11 13:03:09 +1000

    Added Users List

--- client/src/components/pages/ProjectPage/AssignedUsers/AssignedUsers.tsx
+++ client/src/components/pages/ProjectPage/AssignedUsers/AssignedUsers.tsx
@@ -6,15 +6,20 @@ import './AssignedUsers.scss';
 // Import Icons
 import { FaPlus } from 'react-icons/fa';
 
+// Import Components
+import UsersList from './UsersList/UsersList';
+
 const AssignedUsers = ({ project }: any) => {
     return (
         <div className="assigned-users">
 
             {project ? <>
             
-                <h2 className="title">Assigned Users</h2>
+                <h2 className="title">Assigned Users ({project.usersList.length + 1})</h2>
                 <button className="btn primary add-btn"><FaPlus className="icon" /></button>
-                
+                <p className="sub-text">A list of users assigned to this project</p>
+                <UsersList users={[project.owner, ...project.usersList]} project={project} />
+
             </> : ''}
 
         </div>
