af007e9a91ec8375d38324e92aafd922a38128eb
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-11 23:33:18 +1000

    Created Tickets Component

---
+++ client/src/components/pages/ProjectPage/Tickets/Tickets.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// Import Styling
+import './Tickets.scss';
+
+const Tickets = ({ project }: any) => {
+
+    return (
+
+        <div className="ticket-section">
+
+            <h2 className="title">Tickets</h2>
+            
+        </div>
+
+    )
+
+}
+
+export default Tickets
