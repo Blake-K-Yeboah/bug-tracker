95f51a2e5540166dca4c7ab60e53f0e61ae96733
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-11 13:10:01 +1000

    Added Support for many users

--- client/src/components/pages/ProjectPage/AssignedUsers/UsersList/UsersList.tsx
+++ client/src/components/pages/ProjectPage/AssignedUsers/UsersList/UsersList.tsx
@@ -7,9 +7,9 @@ import User from './User/User';
 import './UsersList.scss';
 
 const UsersList = ({ users, project }: any) => {
-    
+
     return (
-        <div className="users-list">
+        <div className={`users-list ${users.length > 6 ? 'more-than-six' : ''}`}>
             {users.map((userId: string) => {
                 return <User userId={userId} project={project} key={userId} />
             })}
