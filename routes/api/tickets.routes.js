192aeae4c167304e633e4e1293c3f0b049690c72
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-12 21:42:48 +1000

    Fixed Bug where ticket would still save even if error

--- routes/api/tickets.routes.js
+++ routes/api/tickets.routes.js
@@ -18,11 +18,15 @@ const Change = require('../../models/change.model');
 // Import Validation Function
 const validateTicketInput = require('../../validation/ticket');
 
-router.get('/:id?', jwt({ secret: keys.secretOrKey, algorithms: ['HS256'] }), (req, res) => {
+router.use(jwt({ secret: keys.secretOrKey, algorithms: ['HS256'] }));
+
+router.get('/:id?', (req, res) => {
 
     // Define ID
     const id = req.params.id;
 
+    console.log("Hello Tickets");
+    
     if (id) {
         
         // Return ticket with specified id
@@ -46,7 +50,7 @@ router.get('/:id?', jwt({ secret: keys.secretOrKey, algorithms: ['HS256'] }), (r
     }   
 });
 
-router.post('/create', jwt({ secret: keys.secretOrKey, algorithms: ['HS256'] }), (req, res) => {
+router.post('/create', (req, res) => {
 
     // Validation
     const { errors, isValid } = validateTicketInput(req.body);
@@ -55,42 +59,50 @@ router.post('/create', jwt({ secret: keys.secretOrKey, algorithms: ['HS256'] }),
         return res.status(400).json(errors);
     }
 
-    // TODO Check if User in assigned to project
-    Project.findById(req.body.projectId).then(project => {
+    Project.findById(req.body.projectId).then((project) => {
         
-        if (!project) return res.status(400).json({ msg: "No project with that id" });
+        if (!project) {
 
-        if (!project.usersList.includes(req.body.owner)) return res.status(400).json({ msg: "You are not assigned to that project" });
-        
-    });
+            return res.status(400).json({ project: "No project with that ID." });
 
-    // TODO Check if dev is an actual developer
-    // TODO Check if ticket with same text exists
+        } else if (!project.usersList.includes(req.body.owner) && project.owner !== req.body.owner) {
+            
+            return res.status(400).json({ owner: "You are not assigned to that project" });
 
-    const newTicket = new Ticket(req.body);
+        } else {
+            
+            // TODO Check if dev is an actual developer
+            // TODO Check if ticket with same text exists
 
-    newTicket.save().then(ticket => {
+            const newTicket = new Ticket(req.body);
 
-        const properties = {
-            userId: req.body.owner,
-            ticketText: ticket.text
+            newTicket.save().then(ticket => {
+
+                const properties = {
+                    userId: req.body.owner,
+                    ticketText: ticket.text
+                }
+    
+                const newChange = new Change({
+                    message: "created a ticket ",
+                    type: "TICKET_CREATED",
+                    properties: JSON.stringify(properties)
+                });
+    
+                newChange.save().then(change => { }).catch(err => console.log(err));
+                
+                return res.json(ticket);
+    
+            }).catch(err => {
+    
+                return res.status(500).json(err);
+    
+            });
+
         }
 
-        const newChange = new Change({
-            message: "created a ticket ",
-            type: "TICKET_CREATED",
-            properties: JSON.stringify(properties)
-        });
-
-        newChange.save().then(change => { }).catch(err => console.log(err));
-
-        return res.json(ticket);
-
-    }).catch(err => {
-
-        return res.status(500).json(err);
-
     });
+
 
 });
 
