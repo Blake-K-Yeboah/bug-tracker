216a04d636eb709a8a9809194e0c25de7a770174
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-13 19:50:51 +1000

    Added Ticket Length to Title

--- client/src/components/pages/ProjectPage/Tickets/Tickets.tsx
+++ client/src/components/pages/ProjectPage/Tickets/Tickets.tsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 // Import Styling
 import './Tickets.scss';
 
-const Tickets = ({ project }: any) => {
+// Import MobX Stuff
+import { inject, observer } from 'mobx-react';
+
+// Import Types
+import { IStoreProps, Iticket } from '../../../../types';
+
+let Tickets = ({ project, ticketStore }: IStoreProps) => {
+
+    useEffect(() => {
+        ticketStore.fetchTickets();
+    }, [ticketStore]);
+
+    const tickets: Iticket[] = ticketStore.tickets.length > 0 && project ? ticketStore.tickets.filter((ticket: Iticket) => ticket.projectId === project._id) : [];
 
     return (
 
         <div className="ticket-section">
 
-            <h2 className="title">Tickets</h2>
+            <h2 className="title">Tickets ({tickets.length})</h2>
             
         </div>
 
     )
 
 }
+
+Tickets = inject('ticketStore')(observer(Tickets));
 
 export default Tickets
