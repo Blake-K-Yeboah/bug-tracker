af9fce78dc4ceed08c750c6eeba1da128da57902
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-13 21:02:52 +1000

    Added Taable Row for each ticket

--- client/src/components/pages/ProjectPage/Tickets/TicketTable/TicketTable.tsx
+++ client/src/components/pages/ProjectPage/Tickets/TicketTable/TicketTable.tsx
@@ -3,10 +3,46 @@ import React from 'react'
 // Import Stylesheet
 import './TicketTable.scss'
 
+// Import types
+import { Iticket } from '../../../../../types'
+
+// Import Compoennts
+import TableRow from './TableRow'
+
 const TicketTable = ({ tickets }: any) => {
+
     return (
         <table className="ticket-table">
-            Ticket Table
+            
+            <thead>
+
+                <tr className="head-row">
+                    <th className="t-head">
+                        Text
+                    </th>
+                    <th className="t-head">
+                        Owner
+                    </th>
+                    <th className="t-head">
+                        Status
+                    </th>
+                    <th className="t-head">
+                        View
+                    </th>
+                </tr>
+
+            </thead>
+
+            <tbody>
+
+                {tickets.map((ticket: Iticket) => {
+
+                    return <TableRow ticket={ticket} key={ticket._id} />
+
+                })}
+
+            </tbody>
+
         </table>
     )
 }
