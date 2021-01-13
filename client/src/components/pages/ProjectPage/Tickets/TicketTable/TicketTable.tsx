6bd013063dcdd5356aee917571adf5083b9e0241
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-13 19:53:14 +1000

    Created Ticket Table component

---
+++ client/src/components/pages/ProjectPage/Tickets/TicketTable/TicketTable.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Import Stylesheet
+import './TicketTable.scss'
+
+const TicketTable = ({ tickets }: any) => {
+    return (
+        <table className="ticket-table">
+            Ticket Table
+        </table>
+    )
+}
+
+export default TicketTable
