33a28ffc5287d3518bb06d682125092a4a5dd167
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-18 15:11:38 +1000

    Created RemoveUsersList Component

---
+++ client/src/components/pages/ManageProjectUsers/RemoveUsers/RemoveUsersList/RemoveUsersList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Import Styling
+import './RemoveUsersList.scss';
+
+const RemoveUsersList = ({ project }: any) => {
+    return (
+        <ul className="remove-users-list">
+            <li>Hello</li>
+        </ul>
+    )
+}
+
+export default RemoveUsersList;
