d32f087b741edc858bbee28499e5a032887fefb3
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-19 19:26:31 +1000

    Created Add Users List Component

---
+++ client/src/components/pages/ManageProjectUsers/AddUsers/AddUsersList/AddUsersList.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react'
+
+// Import Styling
+import './AddUsersList.scss';
+
+// Import Components
+import ListItem from './ListItem/ListItem';
+
+// Import MobX Stuff
+import { inject, observer } from 'mobx-react';
+
+// Import types
+import { IStoreProps, Iuser } from '../../../../../types';
+
+let AddUsersList = ({ project, usersStore }: IStoreProps) => {
+
+    useEffect(() => {
+        usersStore.fetchUsers();
+    }, [usersStore]); 
+
+    const users = usersStore.users.filter((user: Iuser) => !project.usersList.includes(user._id) && user._id !== project.owner);
+
+    return (
+        <ul className="add-users-list">
+            
+            {users.map((user: Iuser) => (
+                <ListItem userId={user._id} key={user._id} />
+            ))}
+
+            {users.length === 0 ? <p style={{marginLeft: '1.75em'}}>No Users</p> : ''}
+
+        </ul>
+    )
+}
+
+AddUsersList = inject('usersStore')(observer(AddUsersList));
+
+export default AddUsersList;
