64f2c561d19ca8e1f921b979601ae65253de0bfc
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-20 20:40:38 +1000

    Created Edit Profile Component

---
+++ client/src/components/pages/EditProfile/EditProfile.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react'
+
+// Import Helmet to access document head
+import { Helmet } from 'react-helmet'
+
+// Import Styling
+import './EditProfile.scss';
+
+// Import Page Components
+import Sidebar from '../../layout/Sidebar/Sidebar'
+import Navbar from '../../layout/Navbar/Navbar';
+
+// Import Axios
+import Axios from 'axios';
+
+// Import NavLink
+import { NavLink } from 'react-router-dom';
+
+const EditProfile = ({ match: { params: { id }}}: any) => {
+    
+    const [user, setUser]: any = useState(null);
+
+    useEffect(() => {
+
+        Axios.get(`/api/users/${id}`).then(res => {
+            setUser(res.data);
+        });
+
+    }, [id]);
+
+    
+    return (
+        <>
+            <Helmet>
+
+                <title>Bug Tracker - Edit User Profile</title>
+
+            </Helmet>
+
+            <div className="page-content">
+
+                <Navbar />
+
+                <Sidebar />
+
+                <h1 className="page-title">Editing User Profile - {user ? user.name : ''}</h1>
+                
+                <div className="edit-project-page-grid">
+
+                    <div className="placeholder"></div>
+                    <div className="placeholder"></div>
+                    <div className="placeholder"></div>
+                    <div className="placeholder"></div>
+
+                </div>
+
+                <NavLink to={`/profile/${id}`} className="page-link">&lt; Back to profile</NavLink>
+                
+            </div>
+
+        </>
+    )
+
+}
+
+export default EditProfile
