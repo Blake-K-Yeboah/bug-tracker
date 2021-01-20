8e361ddd917408c6cfb3937fec722f17d39f156f
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-20 21:25:31 +1000

    Created Edit Details Component

---
+++ client/src/components/pages/EditProfile/EditDetails/EditDetails.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// Import Styling
+import './EditDetails.scss';
+
+const EditDetails = ({ user }: any) => {
+    return (
+        <div className="edit-details">
+            
+            <h2 className="title">Edit Details</h2>
+
+        </div>
+    )
+}
+
+export default EditDetails
