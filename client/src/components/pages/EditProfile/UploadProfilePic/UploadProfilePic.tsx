b0db0fb56835dea60b0ed51e2c69699b331fc374
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-21 15:37:16 +1000

    Created UploadProfilePic Component

---
+++ client/src/components/pages/EditProfile/UploadProfilePic/UploadProfilePic.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// Import Stylesheet
+import './UploadProfilePic.scss';
+
+const UploadProfilePic = ({ user }: any) => {
+
+    return (
+        <div className="upload-profile-pic">
+            <h2 className="title">Upload Profile Picture</h2>
+        </div>
+    )
+
+}
+
+export default UploadProfilePic
