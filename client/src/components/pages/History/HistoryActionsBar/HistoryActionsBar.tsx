7a1adf26df6b031b4ef6ac4ea86f39cfc4f49fba
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-24 21:12:02 +1000

    Added Sorting Changes Functionality

--- client/src/components/pages/History/HistoryActionsBar/HistoryActionsBar.tsx
+++ client/src/components/pages/History/HistoryActionsBar/HistoryActionsBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 // Import Style
 import './HistoryActionsBar.scss';
@@ -15,19 +15,30 @@ interface PropsI {
 }
 
 let HistoryActionsBar = ({ changeStore }: PropsI) => {
+
+    useEffect(() => {
+        changeStore!.fetchChanges();
+    }, [changeStore]);
+
+    const sortByHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        changeStore!.changeSort(e.target.value);
+    }
+
     return (
         <div className="history-actions-bar">
             
             <label htmlFor="sort-select">Sort By</label>
 
-            <select className="sort-select" id="sort-select">
+            <select className="sort-select" id="sort-select" value={changeStore!.changesSort} onChange={sortByHandler}>
 
                 <option value="newest">Newest First</option>
+                <option value="oldest">Oldest First</option>
 
             </select>
             
         </div>
     )
+
 }
 
 // Inject Store
