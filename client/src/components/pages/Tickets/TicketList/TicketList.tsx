8e45e2e440cd4ff853ac6e54801769650a996db0
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-27 16:40:10 +1000

    Created Ticket List Component

---
+++ client/src/components/pages/Tickets/TicketList/TicketList.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+// Import Styling
+import './TicketList.scss';
+
+// Import Types
+import { ITicketStore } from '../../../../types';
+
+// Import mobX Stuff
+import { inject, observer } from 'mobx-react';
+
+// Props Interface
+interface PropsI {
+    ticketStore?: ITicketStore
+}
+
+let TicketList = ({ ticketStore }: PropsI) => {
+    return (
+        <ul className="ticket-list">
+            <h2 className="heading">Ticket List</h2>
+        </ul>
+    )
+}
+
+TicketList = inject('ticketStore')(observer(TicketList));
+
+export default TicketList
