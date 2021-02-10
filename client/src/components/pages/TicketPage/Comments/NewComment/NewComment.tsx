99b8469cfbb1e4b4fb07c36e40e9dc5ed4fbec7a
Blake Yeboah <blakekyeboah@gmail.com>
2021-02-10 19:45:37 +1000

    Created new Comment Component

---
+++ client/src/components/pages/TicketPage/Comments/NewComment/NewComment.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+
+// Import Styling
+import './NewComment.scss';
+
+// Import Types
+import { IAuthStore, ICommentStore, Iticket } from '../../../../../types';
+
+// Import MobX Stuff
+import { inject, observer } from 'mobx-react';
+
+// Import Axios
+import Axios from 'axios';
+
+// Props Interface
+interface PropsI {
+    ticket: Iticket | null,
+    authStore?: IAuthStore,
+    commentStore?: ICommentStore
+}
+
+// Request Body Interface
+interface IRequestBody {
+    text:string,
+    userId: string,
+    type: string,
+    typeId: string
+}
+
+let NewComment = ({ ticket, authStore, commentStore }: PropsI) => {
+
+    const [text, setText] = useState<string>('');
+
+    const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setText(e.target.value);
+    }
+
+    const createNewComment = () => {
+
+        const requestBody: IRequestBody = {
+            text,
+            userId: authStore!.user.id,
+            type: 'ticket',
+            typeId: ticket!._id
+        }
+
+        Axios.post('/api/comments/create', requestBody).then(res => {
+            commentStore!.fetchComments();
+            setText('')
+        }).catch(err => {
+            alert(err.response.data.text);
+        });
+
+    };
+
+    return (
+        <div className="new-comment">
+            
+            <h4 className="small-heading">Write a comment</h4>
+
+            <input type="text" className="comment-input" value={text} onChange={inputChangeHandler} placeholder="Comment: " />
+
+            <button className="btn primary" onClick={createNewComment}>Post</button>
+
+        </div>
+    )
+}
+
+// Inject Store
+NewComment = inject('authStore', 'commentStore')(observer(NewComment));
+
+export default NewComment
