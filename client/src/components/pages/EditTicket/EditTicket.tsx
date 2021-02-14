d79e78625642f5dbf9d976ea423b60d25d7849c1
Blake Yeboah <blakekyeboah@gmail.com>
2021-02-14 18:07:02 +1000

    Created Edit Ticket Page

---
+++ client/src/components/pages/EditTicket/EditTicket.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react'
+
+// Import Helmet to access document head
+import { Helmet } from 'react-helmet';
+
+// Import Components
+import Navbar from '../../layout/Navbar/Navbar';
+import Sidebar from '../../layout/Sidebar/Sidebar';
+
+// Import Styling
+import './EditTicket.scss';
+
+// Import Types
+import { Iticket } from '../../../types';
+import Axios from 'axios';
+
+// Props Interface
+interface PropsI {
+   match: {
+       params: {
+           id: string
+       }
+   }
+}
+
+const EditTicket = ({ match: { params: { id }}}: PropsI) => {
+
+    const [ticket, setTicket] = useState<Iticket | null>(null);
+
+    useEffect(() => {
+
+        // Fetch Ticket
+        const fetchTicket = async () => {
+
+            const res = await Axios.get(`/api/tickets/${id}`);
+            setTicket(res.data);
+
+        }
+
+        fetchTicket();
+
+    }, [id]);
+
+    return (
+        <>
+
+            <Helmet>
+
+                <title>Bug Tracker - Edit Ticket</title>
+
+            </Helmet>
+
+            <div className="page-content">
+
+                <Navbar />
+
+                <Sidebar />
+
+                <h1 className="page-title">Editing Ticket "{ticket ? ticket.text : 'Loading'}"</h1>
+
+                <div className="edit-ticket-page-grid">
+
+                    <div className="placeholder"></div>
+                    <div className="placeholder"></div>
+                    <div className="placeholder"></div>
+                    <div className="placeholder"></div>
+
+                </div>
+
+            </div>
+
+        </>
+    )
+}
+
+export default EditTicket
