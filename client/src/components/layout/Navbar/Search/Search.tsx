3569e9287cbee0daf8bdc8c7f36550db7c23573d
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-04 17:44:46 +1000

    Added Functionality To Display Both users and projects at the same time

--- client/src/components/layout/Navbar/Search/Search.tsx
+++ client/src/components/layout/Navbar/Search/Search.tsx
@@ -10,7 +10,7 @@ import { FaFolderOpen } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
 
 // Import Types
-import { Iproject, IProjectStore, Iuser, IUsersStore } from '../../../../types';
+import { IAuthStore, Iproject, IProjectStore, Iuser, IUsersStore } from '../../../../types';
 
 // Import Styling
 import './Search.scss';
@@ -18,7 +18,8 @@ import './Search.scss';
 // Props Interface
 interface PropsI {
   usersStore?: IUsersStore,
-  projectStore?: IProjectStore
+  projectStore?: IProjectStore,
+  authStore?: IAuthStore
 }
 
 // Search Results Interface
@@ -27,15 +28,19 @@ interface ISearchResults {
   users: Iuser[]
 }
 
-let Search = ({ usersStore, projectStore}: PropsI) => {
+// Individual Search Result Interface
+interface IResult {
+  type: string,
+  user?: Iuser,
+  project?: Iproject
+}
+
+let Search = ({ usersStore, projectStore, authStore}: PropsI) => {
 
   // Define State
   const [searchInput, setSearchInput] = useState<string>('');
   const [searchDisplay, setSearchDisplay] = useState<boolean>(false);
   const [searchResults, setSearchResults]  = useState<ISearchResults>({ projects: [], users: [] });
-
-  // Check whether there are more users than projects
-  const userLengthCondition = searchResults.users.length > searchResults.projects.length;
 
   useEffect(() => {
 
@@ -46,11 +51,13 @@ let Search = ({ usersStore, projectStore}: PropsI) => {
     // Check if there is any input
     if (searchInput !== '') {
 
+      // Show Search Results 
       setSearchDisplay(true);
 
+      // Set Search Results
       setSearchResults({ 
         users: usersStore!.users.filter((user: Iuser) => user.name.toLowerCase().startsWith(searchInput.toLowerCase())), 
-        projects: projectStore!.projects.filter((project: Iproject) => project.name.toLowerCase().startsWith(searchInput.toLowerCase()))
+        projects: projectStore!.projects.filter((project: Iproject) => project.name.toLowerCase().startsWith(searchInput.toLowerCase()) && (project.usersList.includes(authStore!.user.id) || project.owner === authStore?.user.id))
       });
 
     } else {
@@ -61,24 +68,33 @@ let Search = ({ usersStore, projectStore}: PropsI) => {
 
     }
 
-  }, [searchInput, usersStore, projectStore]);
+  }, [searchInput, usersStore, projectStore, authStore]);
+
+  // Combine users and projects into one array to be displayed together
+  const combinedSearchResults = [...searchResults.users.map(user => ({ type: 'user', user })), ...searchResults.projects.map(project => ({ type: 'project', project }))]
 
   return (
       <div className="search">
         <input type="text" className={`search-input ${searchDisplay ? 'expand' : ''}`} placeholder="Search" value={searchInput} onChange={e => setSearchInput(e.target.value)} />
         <div className={`search-results ${searchDisplay ? '' : 'hidden'}`}>
           {searchDisplay ? (<>
-          <h3 className="result-heading">Results (Showing {userLengthCondition ? searchResults.users.slice(0,4).length : searchResults.projects.length === 0 && searchResults.users.length === 0 ? '0' : searchResults.projects.slice(0,4).length} of {userLengthCondition ? searchResults.users.length : searchResults.projects.length})</h3>
-            {userLengthCondition ? 
-              searchResults.users.slice(0,4).map((user: Iuser) => <NavLink key={user._id} className="result" to={`/profile/${user._id}`}><img src={`${process.env.PUBLIC_URL}/uploads/profile/${user.profileIcon}`} alt="Profile Icon" className="profile-pic" /><span className="name">{user.name}</span></NavLink>)
-              : searchResults.projects.slice(0,4).map((project: Iproject) => <NavLink key={project._id} className="result" to={`/project/${project._id}`}><FaFolderOpen className="icon" /><span className="name">{project.name}</span></NavLink>)            
-            }
+          <h3 className="result-heading">Results (Showing {combinedSearchResults.slice(0,4).length} of {combinedSearchResults.length})</h3>
+            {combinedSearchResults.slice(0, 4).map((result: IResult) => {
+              if (result.type === "user") {
+                const user = result.user;
+                return <NavLink key={user!._id} className="result" to={`/profile/${user!._id}`}><img src={`${process.env.PUBLIC_URL}/uploads/profile/${user!.profileIcon}`} alt="Profile Icon" className="profile-pic" /><span className="name">{user!.name}</span></NavLink>
+              } else {
+                const project = result.project;
+                return <NavLink key={project!._id} className="result" to={`/project/${project!._id}`}><FaFolderOpen className="icon" /><span className="name">{project!.name}</span></NavLink>
+              }
+            })}
           </>) : ''}
         </div>
       </div>
   )
 }
 
-Search = inject("usersStore", "projectStore")(observer(Search));
+// Injects Stores
+Search = inject("usersStore", "projectStore", "authStore")(observer(Search));
 
 export default Search;
