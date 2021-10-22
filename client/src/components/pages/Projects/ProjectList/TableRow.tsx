66594e8740ce4c4f0c25a4c22a718120a70a4b24
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-22 12:57:00 +1000

    Improved efficiency

--- client/src/components/pages/Projects/ProjectList/TableRow.tsx
+++ client/src/components/pages/Projects/ProjectList/TableRow.tsx
@@ -1,79 +1,82 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 // Import Axios
-import Axios from 'axios';
+import Axios from "axios";
 
 // Import MobX Stuff
-import { inject, observer } from 'mobx-react';
+import { inject, observer } from "mobx-react";
 
 // Import NavLink
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 // Import Types
-import { Iproject, IProjectStore } from '../../../../types';
+import { Iproject, IProjectStore, Iuser, IUsersStore } from "../../../../types";
 
 // Props Interface
 interface PropsI {
-    project: Iproject,
-    projectStore?: IProjectStore
+    project: Iproject;
+    projectStore?: IProjectStore;
+    usersStore?: IUsersStore;
 }
 
-let TableRow = ({ project, projectStore }: PropsI ) => {
-
-    const [owner, setOwner]: any = useState(null);
+let TableRow = ({ project, projectStore, usersStore }: PropsI) => {
+    const [owner, setOwner]: any = useState<Iuser | {}>(
+        usersStore!.users
+            ? usersStore!.users.filter((user) => user._id === project.owner)[0]
+            : {}
+    );
 
     useEffect(() => {
-        let _isMounted = true;
-
-        // Fetch User
-        const fetchUser = async () => {
-            const res = await Axios.get(`/api/users/${project.owner}`);
-            setOwner(res.data);
-        }
-
-        if (_isMounted) {
-
-            fetchUser()
-        
-        }
-
-    }, [project.owner]);
+        usersStore!.fetchUsers();
+    }, [usersStore]);
 
     const deleteHandler = () => {
-        Axios.delete(`/api/projects/${project._id}`).then(res => {
-            projectStore!.fetchProjects();
-            window.location.reload();
-        }).catch(err => {
-            alert('An Error Occured');
-        });
+        Axios.delete(`/api/projects/${project._id}`)
+            .then((res) => {
+                projectStore!.fetchProjects();
+                window.location.reload();
+            })
+            .catch((err) => {
+                alert("An Error Occured");
+            });
     };
 
     return (
         <tr className="row">
-            <td className="table-data">
-                {project.name}
-            </td>
-            <td className="table-data">
-                {project.description}
-            </td>
+            <td className="table-data">{project.name}</td>
+            <td className="table-data">{project.description}</td>
             <td className="table-data owner-td">
-                {owner ? <>
-                    <img src={`${process.env.PUBLIC_URL}/uploads/profile/${owner.profileIcon}`} className="profileIcon" alt="Profile Icon" />
-                    <NavLink className="name" to={`/profile/${owner._id}`}>{owner.name}</NavLink> </> 
-                : <div className="loader"></div>}
+                {owner ? (
+                    <>
+                        <img
+                            src={`${process.env.PUBLIC_URL}/uploads/profile/${owner.profileIcon}`}
+                            className="profileIcon"
+                            alt="Profile Icon"
+                        />
+                        <NavLink className="name" to={`/profile/${owner._id}`}>
+                            {owner.name}
+                        </NavLink>{" "}
+                    </>
+                ) : (
+                    <div className="loader"></div>
+                )}
             </td>
             <td className="table-data">
-                <NavLink className="btn-link-container" to={`/project/${project._id}`}>
+                <NavLink
+                    className="btn-link-container"
+                    to={`/project/${project._id}`}
+                >
                     <button className="btn primary">View Project</button>
                 </NavLink>
-                <button className="btn danger" onClick={deleteHandler}>Delete Project</button>
+                <button className="btn danger" onClick={deleteHandler}>
+                    Delete Project
+                </button>
             </td>
         </tr>
-    )
-
-}
+    );
+};
 
 // Inject Store
-TableRow = inject('projectStore')(observer(TableRow));
+TableRow = inject("projectStore", "usersStore")(observer(TableRow));
 
-export default TableRow
+export default TableRow;
