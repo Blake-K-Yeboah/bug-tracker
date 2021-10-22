13f883fa5a5318a02afefa85277fdcdfb0c06782
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-22 12:21:36 +1000

    Fixed Bug

--- client/src/components/pages/Dashboard/LatestActivity/LatestActivity.tsx
+++ client/src/components/pages/Dashboard/LatestActivity/LatestActivity.tsx
@@ -1,47 +1,48 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 // Import Styling
-import './LatestActivity.scss';
+import "./LatestActivity.scss";
 
 // Import Mobx stuff
-import { inject, observer } from 'mobx-react';
+import { inject, observer } from "mobx-react";
 
 // Import Types
-import { IChangeStore } from '../../../../types';
+import { IChangeStore } from "../../../../types";
 
 // Import Page Components
-import LatestActivityList from './LatestActivityList/LatestActivityList';
+import LatestActivityList from "./LatestActivityList/LatestActivityList";
 
 // Import NavLink
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 // Props Interface
 interface PropsI {
-    changeStore?: IChangeStore
+    changeStore?: IChangeStore;
 }
 
 let LatestActivity = ({ changeStore }: PropsI) => {
-
     useEffect(() => {
         changeStore!.fetchChanges();
     }, [changeStore]);
 
     return (
         <div className="latest-activity-container">
-
             <h3 className="heading">Latest Activity</h3>
 
             <LatestActivityList />
 
-            {
-                changeStore && changeStore.changeCount > 7 ? <NavLink to="/history" className="view-all-link">View All</NavLink> : <span className="disabled-view-all-link">View All</span>
-            }
-
-        </div>  
-    )
-}
+            {changeStore && changeStore.changeCount > 7 ? (
+                <NavLink to="/history" className="view-all-link">
+                    View All
+                </NavLink>
+            ) : (
+                <span className="disabled-view-all-link">View All</span>
+            )}
+        </div>
+    );
+};
 
 // Inject Store
 LatestActivity = inject("changeStore")(observer(LatestActivity));
 
-export default LatestActivity
+export default LatestActivity;
