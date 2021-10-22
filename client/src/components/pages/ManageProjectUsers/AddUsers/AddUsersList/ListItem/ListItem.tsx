20eb8586ca2d13c27505ca8ed24dd56e7c4a01ae
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-22 12:52:12 +1000

    Improved efficiency

--- client/src/components/pages/ManageProjectUsers/AddUsers/AddUsersList/ListItem/ListItem.tsx
+++ client/src/components/pages/ManageProjectUsers/AddUsers/AddUsersList/ListItem/ListItem.tsx
@@ -1,85 +1,94 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 // Import Styling
-import './ListItem.scss';
+import "./ListItem.scss";
 
 // Import Axios
-import Axios from 'axios';
+import Axios from "axios";
 
 // Import NavLink
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 // Import MobX Stuff
-import { inject, observer } from 'mobx-react';
+import { inject, observer } from "mobx-react";
 
 // Import types
-import { IAuthStore, Iuser } from '../../../../../../types';
+import { IAuthStore, Iuser, IUsersStore } from "../../../../../../types";
 
 // Props Interface
-interface PropsI {
-    userId: string,
-    authStore?: IAuthStore,
-    projectId: string
+interface IProps {
+    userId: string;
+    authStore?: IAuthStore;
+    projectId: string;
+    usersStore?: IUsersStore;
 }
 
 // Request Body Interface
 interface IRequestBody {
-    userId: string,
-    addedUserId: string
+    userId: string;
+    addedUserId: string;
 }
 
-let ListItem = ({ userId, authStore, projectId }: PropsI) => {
-
-    const [user, setUser]: any = useState<Iuser | string>('');
+let ListItem = ({ userId, authStore, projectId, usersStore }: IProps) => {
+    const [user, setUser]: any = useState<Iuser | {}>(
+        usersStore!.users
+            ? usersStore!.users.filter((user) => user._id === userId)[0]
+            : {}
+    );
 
     useEffect(() => {
-
-        // Fetch User
-        const fetchUser = async () => {
-            const res = await Axios.get(`/api/users/${userId}`);
-            setUser(res.data)
-        }
-        
-        fetchUser();
-
-    }, [userId]);
+        usersStore!.fetchUsers();
+    }, [usersStore]);
 
     const addUserHandler = () => {
         const body: IRequestBody = {
             userId: authStore!.user.id,
-            addedUserId: user._id
-        }
+            addedUserId: user._id,
+        };
 
-        Axios.put(`/api/projects/${projectId}/adduser`, body).then(res => {
-            window.location.reload();
-        }).catch(err => {
-            alert("An error Occured")
-        })
-    }
+        Axios.put(`/api/projects/${projectId}/adduser`, body)
+            .then((res) => {
+                window.location.reload();
+            })
+            .catch((err) => {
+                alert("An error Occured");
+            });
+    };
 
     return (
         <>
-            {user ? <>
-                <li className="list-item">
-                    
-                        
-                        <NavLink to={`/profile/${user._id}`} className="content">
+            {user ? (
+                <>
+                    <li className="list-item">
+                        <NavLink
+                            to={`/profile/${user._id}`}
+                            className="content"
+                        >
+                            <img
+                                className="profile-icon"
+                                src={`${process.env.PUBLIC_URL}/uploads/profile/${user.profileIcon}`}
+                                alt="Profile Icon"
+                            />
 
-                            <img className="profile-icon" src={`${process.env.PUBLIC_URL}/uploads/profile/${user.profileIcon}`} alt="Profile Icon" />                
-                            
                             <p className="name">{user.name}</p>
-
                         </NavLink>
 
-                        <button className="btn primary" onClick={addUserHandler}>Add</button>
-                    
-                </li>
-            </> : ''}
+                        <button
+                            className="btn primary"
+                            onClick={addUserHandler}
+                        >
+                            Add
+                        </button>
+                    </li>
+                </>
+            ) : (
+                ""
+            )}
         </>
-    )
-}
+    );
+};
 
 // Inject Store
-ListItem = inject('authStore')(observer(ListItem));
+ListItem = inject("authStore", "usersStore")(observer(ListItem));
 
-export default ListItem
+export default ListItem;
