ac04f942c4d8d2b532ed639d4c99829b0018fd79
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-22 12:21:52 +1000

    Fixed Bug so that when value is 0 number still shows

--- client/src/components/pages/Dashboard/Statistics/StatBox.tsx
+++ client/src/components/pages/Dashboard/Statistics/StatBox.tsx
@@ -1,26 +1,24 @@
-import React from 'react'
+import React from "react";
 
 // Props Interface
 interface PropsI {
-    title: string,
-    value: number | undefined,
-    children: JSX.Element
+    title: string;
+    value: number | undefined;
+    children: JSX.Element;
 }
 
 const StatBox = ({ title, value, children }: PropsI) => {
-
-
     return (
         <div className="stat-box">
-            <div className="icon-section">
-                {children /* Icon */}
-            </div>
+            <div className="icon-section">{children /* Icon */}</div>
             <div className="text-section">
                 <h3 className="title">{title as string}</h3>
-                <h2 className="value">{value ? value as number : '...'}</h2>
+                <h2 className="value">
+                    {value || value === 0 ? (value as number) : "..."}
+                </h2>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default StatBox
+export default StatBox;
