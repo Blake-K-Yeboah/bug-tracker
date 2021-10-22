ef790ce008f9ce305015fb598ade6c146eba063f
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-22 12:39:23 +1000

    Fixed Bug With Percentage being NaN%

--- client/src/components/pages/Tickets/TicketsChart/Legend/Legend.tsx
+++ client/src/components/pages/Tickets/TicketsChart/Legend/Legend.tsx
@@ -1,46 +1,51 @@
-import React from 'react'
+import React from "react";
 
 // Import Styling
-import './Legend.scss';
+import "./Legend.scss";
 
 interface PropsI {
     values: {
         notStarted: number;
         inProgress: number;
         completed: number;
-    }
+    };
 }
 
 const Legend = ({ values }: PropsI) => {
-
     // Total amount of tickets to determine percentage of each
     const total = Object.values(values).reduce((total, value) => total + value);
 
     // Percentage Values to Output
-    const percentageValues = Object.values(values).map(value => Math.round((value / total) * 1000) / 10); // Round to one decimal place for ease of reading
+    const percentageValues = Object.values(values).map(
+        (value) => Math.round((value / total) * 1000) / 10
+    ); // Round to one decimal place for ease of reading
 
     return (
         <div className="legend">
-
             <h4 className="title">Legend</h4>
 
             <div className="pair">
                 <div className="key red"></div>
-                <span className="value">Not Started ({percentageValues[0]}%)</span>
+                <span className="value">
+                    Not Started {total > 0 ? `(${percentageValues[0]}%)` : ""}
+                </span>
             </div>
 
             <div className="pair">
                 <div className="key orange"></div>
-                <span className="value">In Progress ({percentageValues[1]}%)</span>
+                <span className="value">
+                    In Progress {total > 0 ? `(${percentageValues[1]}%)` : ""}
+                </span>
             </div>
 
             <div className="pair">
                 <div className="key green"></div>
-                <span className="value">Completed ({percentageValues[2]}%)</span>
+                <span className="value">
+                    Completed {total > 0 ? `(${percentageValues[2]}%)` : ""}
+                </span>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default Legend
+export default Legend;
